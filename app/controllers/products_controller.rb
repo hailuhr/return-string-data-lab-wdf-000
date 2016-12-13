4f5b3dcc463e917ff0a6461f479e13db7fe9afa7
Flatiron School <github email address>
2016-12-13 13:41:24 -0500

    done

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,45 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @product = Product.find_by_id(params[:id])
+  end
+
+  def description
+    product = Product.find_by_id(params[:id])
+    render plain: product.description
+  end
+
+  def inventory
+    product = Product.find_by_id(params[:id])
+    # binding.pry
+    if product.inventory >= 1
+      render plain: "true"
+    else
+      render plain: "false"
+    end
+  end
+
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :description, :inventory)
+  end
 end
